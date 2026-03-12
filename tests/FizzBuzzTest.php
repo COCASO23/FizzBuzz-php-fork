00528f2953c14d2ee825fa87acbff2a0c66eb6c6
Carlos Roura <roura.161754@e.unavarra.es>
2026-03-12 18:17:53 +0100

    [RED] - Arreglar estructura test de conseguir 'Hello, World!'.

--- tests/FizzBuzzTest.php
+++ tests/FizzBuzzTest.php
@@ -1,8 +1,13 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Deg540\CleanCodeKata9\Test;
 
-class FizzBuzzTest
+use Deg540\CleanCodeKata9\Example;
+use PHPUnit\Framework\TestCase;
+
+final class ExampleTest extends TestCase
 {
     /**
      * @test
@@ -16,3 +21,12 @@ class FizzBuzzTest
         $this->assertEquals("Hello, World!", $result);
     }
 }
+
+
+
+
+
+
+
+
+
