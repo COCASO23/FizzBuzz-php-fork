e26983fab03acc85a9b1163850744930df1583c1
Carlos Roura <roura.161754@e.unavarra.es>
2026-03-12 18:27:18 +0100

    [GREEN] - Pasar test conseguir 'Hello, World!'.

---
+++ src/FizzBuzz.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Deg540\CleanCodeKata9;
+
+class FizzBuzz
+{
+
+    public function __construct()
+    {
+    }
+    function handler(int $value)
+    {
+        return "Hello, World!";
+    }
+}
--- tests/FizzBuzzTest.php
+++ tests/FizzBuzzTest.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Deg540\CleanCodeKata9\Test;
 
-use Deg540\CleanCodeKata9\Example;
+use Deg540\CleanCodeKata9\FizzBuzz;
 use PHPUnit\Framework\TestCase;
 
 final class FizzBuzzTest extends TestCase
