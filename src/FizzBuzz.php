95cc195644363328492856563e5e98e643cab470
Carlos Roura <roura.161754@e.unavarra.es>
2026-03-12 19:44:21 +0100

    [GREEN] - Crear código para dar número 9 y obtener 'Fizz'.

--- src/FizzBuzz.php
+++ src/FizzBuzz.php
@@ -6,7 +6,7 @@ class FizzBuzz
 {
     function handler(int $number):string
     {
-        if($number === 3 or $number === 6)
+        if($number === 3 or $number === 6 or  $number === 9)
         {
             return "Fizz";
         }
