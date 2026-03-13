108aa69b6eb03e6d759d07230037cea0d0c7d80b
Carlos Roura <roura.161754@e.unavarra.es>
2026-03-13 17:29:04 +0100

    [GREEN] - Implementar código para pasar el test que dado el número 30 devolver 'FizzBuzz'.

--- src/FizzBuzz.php
+++ src/FizzBuzz.php
@@ -6,7 +6,7 @@ class FizzBuzz
 {
     function handler(int $number):string
     {
-        if($number === 15)
+        if($number === 15 or $number === 30)
         {
             return "FizzBuzz";
         }
